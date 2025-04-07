92d755f4bab19f16b7b2e5ae34d1f91a5f3d3074
Y0urMome <treyblue14@gmail.com>
2025-04-06 20:32:00 -0400

    I dont understand

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from arsenal import Arsenal
 
 class AlienInvasion:
 
@@ -22,7 +23,7 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
-        self.ship = Ship(self)
+        self.ship = Ship(self, Arsenal(self))
     
     def run_game(self):
         # Game loop
@@ -53,6 +54,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self.ship.fire()
         elif event.key == pygame.K_q:
             self.running = False
             pygame.quit()
