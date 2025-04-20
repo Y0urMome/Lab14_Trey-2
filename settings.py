7e2cbff7651483cd78247a0c632377c52661c27b
Y0urMome <treyblue14@gmail.com>
2025-04-20 17:04:00 -0400

    Part 3 - Created button class and settings for the button and fonts

--- settings.py
+++ settings.py
@@ -29,3 +29,12 @@ class Settings:
         self.alien_h = 30
         self.fleet_direction = 1
         self.fleet_drop_speed = 30
+
+        self.button_w = 200
+        self.button_h = 50
+        self.button_color = (0,0,250)
+
+        self.text_color = (255,255,255)
+        self.button_font_size = 40
+        self.HUD_font_size = 20
+        self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf'
